947e131824f977b36632c316085d38566433778c
jagatb880 <jagatb880@gmail.com>
2022-01-18 10:31:10 +0530

    signup login design

--- src/app/pages/signup-login/signup-login.page.ts
+++ src/app/pages/signup-login/signup-login.page.ts
@@ -25,24 +25,28 @@ export class SignupLoginPage implements OnInit {
       Validators.pattern('^([A-Za-z ]+ )+[A-Za-z ]+$|^[A-Za-z ]+$')])],
       dnstEmail: ["", Validators.required, Validators.email],
       dnstPhoneNumber: ["", Validators.compose([Validators.required,
-        Validators.minLength(10),
-        Validators.pattern('^([0|\+[0-9]{1,5})?([6-9][0-9]{9})$')])],
+      Validators.minLength(10),
+      Validators.pattern('^([0|\+[0-9]{1,5})?([6-9][0-9]{9})$')])],
       dnstRegistrationNumber: ["", Validators.required],
       clinicName: [""],
       clinicAddress: [""],
       clinicPhoneNumber: ["", Validators.compose([Validators.minLength(8),
-        Validators.pattern('^[-0-9\.\-\/]+$')])],
+      Validators.pattern('^[-0-9\.\-\/]+$')])],
       yearOfExp: ["", Validators.compose([Validators.minLength(1),
-        Validators.pattern('^[0-9\.\-\/]+$')])],
+      Validators.pattern('^[0-9\.\-\/]+$')])],
       numberOfChair: ["", Validators.compose([Validators.minLength(1),
-        Validators.pattern('^[0-9\.\-\/]+$')])],
+      Validators.pattern('^[0-9\.\-\/]+$')])],
       dnstGender: [1]
     });
     // this.loginForm.valueChanges.subscribe(data => this.onValueChanged(data));
   }
 
-  segmentChanged(event){
+  segmentChanged(event) {
     console.log(event)
+  }
+
+  openDeveloper() {
+    window.open('https://play.google.com/store/apps/details?id=com.savart', '_system', 'location=yes');
   }
 
 }
